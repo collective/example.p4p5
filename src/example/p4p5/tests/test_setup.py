b46d1b7cbc6f52a172e44e200f912dccdff9927e
hvelarde <hector.velarde@gmail.com>
2016-01-22 10:17:41 -0200

    Add tests for CSS and JS resources

--- src/example/p4p5/tests/test_setup.py
+++ src/example/p4p5/tests/test_setup.py
@@ -3,10 +3,16 @@
 from example.p4p5.testing import EXAMPLE_P4P5_INTEGRATION_TESTING  # noqa
 from plone import api
 
-import unittest2 as unittest
+import unittest
+
+PROJECTNAME = 'example.p4p5'
+
+CSS = '++resource++example.p4p5/chart.css'
+JS = '++resource++example.p4p5/chart.js'
 
 
-class TestSetup(unittest.TestCase):
+class InstallTestCase(unittest.TestCase):
+
     """Test that example.p4p5 is properly installed."""
 
     layer = EXAMPLE_P4P5_INTEGRATION_TESTING
@@ -14,19 +20,47 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.qi = api.portal.get_tool('portal_quickinstaller')
 
     def test_product_installed(self):
         """Test if example.p4p5 is installed with portal_quickinstaller."""
-        self.assertTrue(self.installer.isProductInstalled('example.p4p5'))
-
-    def test_uninstall(self):
-        """Test if example.p4p5 is cleanly uninstalled."""
-        self.installer.uninstallProducts(['example.p4p5'])
-        self.assertFalse(self.installer.isProductInstalled('example.p4p5'))
+        self.assertTrue(self.qi.isProductInstalled(PROJECTNAME))
 
     def test_browserlayer(self):
         """Test that IExampleP4P5Layer is registered."""
         from example.p4p5.interfaces import IExampleP4P5Layer
         from plone.browserlayer import utils
         self.assertIn(IExampleP4P5Layer, utils.registered_layers())
+
+    def test_css_resources(self):
+        css_resource_ids = self.portal.portal_css.getResourceIds()
+        self.assertIn(CSS, css_resource_ids)
+
+    def test_js_resources(self):
+        js_resource_ids = self.portal.portal_javascripts.getResourceIds()
+        self.assertIn(JS, js_resource_ids)
+
+
+class UninstallTestCase(unittest.TestCase):
+
+    """Test that example.p4p5 is properly uninstalled."""
+
+    layer = EXAMPLE_P4P5_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.qi = self.portal['portal_quickinstaller']
+
+        with api.env.adopt_roles(['Manager']):
+            self.qi.uninstallProducts(products=[PROJECTNAME])
+
+    def test_uninstall(self):
+        self.assertFalse(self.qi.isProductInstalled(PROJECTNAME))
+
+    def test_uninstall_css_resources(self):
+        css_resource_ids = self.portal.portal_css.getResourceIds()
+        self.assertNotIn(CSS, css_resource_ids)
+
+    def test_uninstall_js_resources(self):
+        js_resource_ids = self.portal.portal_javascripts.getResourceIds()
+        self.assertNotIn(JS, js_resource_ids)
